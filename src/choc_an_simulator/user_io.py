b4fd63d90f5e4a23c753dba322558b3165a59f7f
Danny Love <feldhaus@cecs.pdx.edu>
2023-11-08 17:06:33 -0800

    prompt_menu_options, parametrized testing

--- src/choc_an_simulator/user_io.py
+++ src/choc_an_simulator/user_io.py
@@ -4,21 +4,57 @@ Functions related to interaction with the terminal.
 This includes both user input and formatted display.
 """
 
-from typing import Optional
+from typing import Optional, List, Tuple
 
 
-def prompt_int(message: str, char_limit: range) -> Optional[int]:
+def prompt_menu_options(message: str, choices: List[str]) -> Optional[Tuple[int, str]]:
+    """
+    Prompt the user to choose from a list of options.
+
+    Args-
+        message: Message to display before menu options.
+
+    Returns-
+        Tuple[int, str]: Tuple containin the choice index and text
+
+    Raises-
+        ArgumentError: choices is empty
+    """
+    if len(choices) == 0:
+        raise ValueError("'choices' may not be empty.")
+    print(message)
+    for i, choice in enumerate(choices):
+        print(f"\t{i+1}: {choice}")
+    selection = prompt_int(
+        message="Selection",
+        char_limit=None,
+        numeric_limit=range(1, len(choices)),
+    )
+    if selection is None:
+        return None
+    return (selection - 1, choices[selection - 1])
+
+
+def prompt_int(
+    message: str,
+    char_limit: Optional[range] = None,
+    numeric_limit: Optional[range] = None,
+) -> Optional[int]:
     """
     Prompt the user for a number with a character limit. Returns 'None' if 'Ctrl+C' is pressed.
 
     Args-
         message: The message to display to the user.
         char_limit: The range of acceptable # of digits for the input.
+        numeric_limit: The (optional) numeric range of the input
 
     Returns-
         Optional[int]: The user's input if 'Ctrl+C' was not pressed, otherwise None.
     """
     result: Optional[int] = None
+    if numeric_limit is not None:
+        message = f"{message} ({numeric_limit.start}-{numeric_limit.stop})"
+
     while result is None:
         result_text = prompt_str(message, char_limit)
         if result_text is None:  # user pressed ctrl+z
@@ -26,10 +62,18 @@ def prompt_int(message: str, char_limit: range) -> Optional[int]:
         result = _to_int_(result_text)
         if result is None:  # Result could not be converted
             print(f'"{result_text}" is not a valid integer.')
+        elif (numeric_limit is not None) and not (
+            numeric_limit.start <= result <= numeric_limit.stop
+        ):
+            print(
+                f'"{result}" is not in the range ({numeric_limit.start}-{numeric_limit.stop})"'
+            )
+            result = None
+
     return result
 
 
-def prompt_str(message: str, char_limit: range) -> Optional[str]:
+def prompt_str(message: str, char_limit: Optional[range] = None) -> Optional[str]:
     """
     Prompt the user for input with a character limit. Returns 'None' if 'Ctrl+C' is pressed.
 
@@ -44,9 +88,12 @@ def prompt_str(message: str, char_limit: range) -> Optional[str]:
         result: Optional[str] = None
         while result is None:
             result = input(f"{message}: ")
-            if not (char_limit.start <= len(result) <= char_limit.stop):
+            if (char_limit is not None) and not (
+                char_limit.start <= len(result) <= char_limit.stop
+            ):
                 print(
-                    f"Input must be between {char_limit.start} and {char_limit.stop} characters."
+                    "Input must be between "
+                    f"{char_limit.start} and {char_limit.stop} characters long."
                 )
                 result = None
         return result
--- tests/test_user_io.py
+++ tests/test_user_io.py
@@ -1,71 +1,104 @@
 """Tests of the user_io module."""
-from choc_an_simulator.user_io import prompt_str, prompt_int, _to_int_
+import pytest
+from choc_an_simulator.user_io import (
+    prompt_str,
+    prompt_int,
+    prompt_menu_options,
+    _to_int_,
+)
+
+
+class TestPromptMenuOptions:
+    """Tests of the prompt_menu_options function"""
+
+    test_parameters = [
+        (["A", "B", "C"], ["1"], (0, "A")),
+        (["A", "B", "C"], ["0", "2"], (1, "B")),
+        (["A", "B", "C"], ["-1", "3"], (2, "C")),
+    ]
+
+    @pytest.mark.parametrize("choices,input_strs,expected", test_parameters)
+    def test_prompt_menu_options_valid_choice(
+        self, choices, input_strs, expected, monkeypatch
+    ):
+        """Test prompt_menu_options using a list of parameters"""
+        inputs = iter(input_strs)
+        monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+        result = prompt_menu_options("Choose an option:", choices)
+        assert result == expected
+
+    def test_prompt_menu_options_empty_choices(self):
+        """Test that an empty 'choices' list raises a ValueError."""
+        with pytest.raises(ValueError):
+            prompt_menu_options("Choose an option:", [])
+
+    def test_prompt_menu_options_ctrl_c(self, monkeypatch):
+        """Test that pressing 'Ctrl+C' returns None."""
+        monkeypatch.setattr("builtins.input", lambda _: exec("raise KeyboardInterrupt"))
+        assert prompt_menu_options("Choose an option:", ["Option 1"]) is None
 
 
 class TestToInt:
     """Test of the _to_int_ function"""
 
-    def test_to_int_valid(self):
-        assert _to_int_("123") == 123
+    test_parameters = [
+        ("123", 123),  # Positive int
+        ("-123", -123),  # Negative int
+        ("1.23", None),  # Decimal
+        ("abc", None),  # Non-numeric text
+        ("", None),  # Empty string
+        ("@#$", None),  # Special characters
+    ]
 
-    def test_to_int_invalid(self):
-        assert _to_int_("abc") is None
-
-    def test_to_int_empty_string(self):
-        assert _to_int_("") is None
-
-    def test_to_int_special_chars(self):
-        assert _to_int_("@#$") is None
+    @pytest.mark.parametrize("input,expected_output", test_parameters)
+    def test_to_int_valid(self, input, expected_output):
+        assert _to_int_(input) == expected_output
 
 
 class TestPromptInt:
-    def test_prompt_int_valid_input(self, monkeypatch):
-        monkeypatch.setattr("builtins.input", lambda x: "123")
-        assert prompt_int("Enter a number", range(1, 4)) == 123
+    test_parameters = [
+        (["1"], None, None),  # Normal input
+        (["10"], range(1, 4), range(9, 12)),  # Input in range
+        (["text", 1], None, None),  # Non-numeric before valid
+        (["-1", "10"], None, range(9, 12)),  # Below numeric range before valid
+        (["20", "10"], None, range(9, 12)),  # Above numeric before valid
+        (["1", "100"], range(2, 4), None),  # Below character limit before valid
+        (["10000", "100"], range(2, 4), None),  # Above character limit before valid
+    ]
 
-    def test_prompt_int_invalid_then_valid(self, monkeypatch, capsys):
-        inputs = iter(["abc", "123"])
-        monkeypatch.setattr("builtins.input", lambda x: next(inputs))
-
-        assert prompt_int("Enter a number", range(1, 4)) == 123
-        captured = capsys.readouterr()
-        assert '"abc" is not a valid integer.' in captured.out
+    @pytest.mark.parametrize("input_strs,char_limit,numeric_limit", test_parameters)
+    def test_prompt_int(self, input_strs, char_limit, numeric_limit, monkeypatch):
+        """Test prompt_int using a list of parameters"""
+        inputs = iter(input_strs)
+        monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+        expected = int(input_strs[-1])
+        assert prompt_int("Enter a number", char_limit, numeric_limit) == expected
 
     def test_prompt_int_ctrl_c(self, monkeypatch):
-        monkeypatch.setattr("builtins.input", lambda x: exec("raise KeyboardInterrupt"))
-        assert prompt_int("Enter a number", range(1, 4)) is None
-
-    def test_prompt_int_char_limit_exceeded_then_valid(self, monkeypatch):
-        inputs = iter(["111111", "11"])
-        monkeypatch.setattr("builtins.input", lambda x: next(inputs))
-        assert prompt_int("Enter a number", range(2, 4)) == 11
-
-    def test_prompt_int_char_limit_below_then_valid(self, monkeypatch):
-        inputs = iter(["1", "11"])
-        monkeypatch.setattr("builtins.input", lambda x: next(inputs))
-        assert prompt_int("Enter a number", range(2, 4)) == 11
+        """Test when the user presses 'Ctrl+C'"""
+        monkeypatch.setattr("builtins.input", lambda _: exec("raise KeyboardInterrupt"))
+        assert prompt_int("Enter a number") is None
 
 
 class TestPromptString:
     """Tests of the prompt_string() function"""
 
-    def test_prompt_string_within_limit(self, monkeypatch):
-        """Test when the user enters a string within the character limit."""
+    test_parameters = [
+        (["test"], None),
+        (["test"], range(1, 4)),
+        (["long input", "test"], range(1, 4)),
+        ([""], range(0)),
+    ]
 
-        inputs = iter(["test"])
-        monkeypatch.setattr("builtins.input", lambda x: next(inputs))
-        assert prompt_str("Enter your input", range(1, 9)) == "test"
-
-    def test_prompt_string_outside_then_within_limit(self, monkeypatch, capsys):
-        """Test when the user initially enters a string outside the character limit."""
-        inputs = iter(["long input", "test"])
-        monkeypatch.setattr("builtins.input", lambda x: next(inputs))
-
-        assert prompt_str("Enter your input", range(1, 9)) == "test"
-        captured = capsys.readouterr()
-        assert "Input must be between 1 and 9 characters." in captured.out
+    @pytest.mark.parametrize("input_strs,char_limit", test_parameters)
+    def test_prompt_string(self, input_strs, char_limit, monkeypatch):
+        """Test prompt_string using a list of parameters"""
+        inputs = iter(input_strs)
+        monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+        # Assert that the last (and only) valid input is returned.
+        assert prompt_str("Input", char_limit) == input_strs[-1]
 
     def test_prompt_string_ctrl_c(self, monkeypatch):
         """Test when the user presses 'Ctrl+C'"""
-        monkeypatch.setattr("builtins.input", lambda x: exec("raise KeyboardInterrupt"))
+        monkeypatch.setattr("builtins.input", lambda _: exec("raise KeyboardInterrupt"))
         assert prompt_str("Enter your input", range(1, 9)) is None
