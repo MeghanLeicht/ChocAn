cf8163c859439df4dcf51a94e81affde3e1941db
Kyle Joseph <kyjoseph@ada.cs.pdx.edu>
2023-11-09 22:36:24 -0800

    Commented the imports for now, and replaced the regular comments to docstrings.

--- src/choc_an_simulator/provider.py
+++ src/choc_an_simulator/provider.py
@@ -1,22 +1,27 @@
-"""Provider Sub-System"""
-from .schemas import MEMBER_INFO, SERVICE_LOG_INFO, PROVIDER_DIRECTORY_INFO
+"""Provider Sub-System."""
+# from .schemas import MEMBER_INFO, SERVICE_LOG_INFO, PROVIDER_DIRECTORY_INFO
+
 
 def show_provider_menu() -> None:
-    # Placeholder for displaying the provider menu
+    """Placeholder for displaying the provider menu."""
     pass
+
 
 def prompt_member_id() -> int:
-   # Placeholder prompting for member_id
-   pass
+    """Placeholder prompting for member_id."""
+    pass
+
 
 def display_member_information(member_id: int) -> None:
-    # Placeholder for displaying member information
+    """Placeholder for displaying member information."""
     pass
+
 
 def record_service_billing_entry(member_id: int) -> None:
-    # Placeholder for recording a service entry
+    """Placeholder for recording a service entry."""
     pass
 
+
 def request_provider_directory() -> None:
-    # Placeholder for requesting a provider directory
+    """Placeholder for requesting a provider directory."""
     pass
