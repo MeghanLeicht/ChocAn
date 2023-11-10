0018545146fbf05a2a4aee4bff8ecdde8b75864f
Danny Love <feldhaus@cecs.pdx.edu>
2023-11-10 12:54:19 -0800

    Changed pass to NotImplementedErrors in placeholder functions

--- src/choc_an_simulator/provider.py
+++ src/choc_an_simulator/provider.py
@@ -4,24 +4,24 @@
 
 def show_provider_menu() -> None:
     """Placeholder for displaying the provider menu."""
-    pass
+    raise NotImplementedError("show_provider_menu")
 
 
 def prompt_member_id() -> int:
     """Placeholder prompting for member_id."""
-    pass
+    raise NotImplementedError("prompt_member_id")
 
 
 def display_member_information(member_id: int) -> None:
     """Placeholder for displaying member information."""
-    pass
+    raise NotImplementedError("display_member_information")
 
 
 def record_service_billing_entry(member_id: int) -> None:
     """Placeholder for recording a service entry."""
-    pass
+    raise NotImplementedError("records_service_billing_entry")
 
 
 def request_provider_directory() -> None:
     """Placeholder for requesting a provider directory."""
-    pass
+    raise NotImplementedError("request_provider_directory")
