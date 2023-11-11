942e0eb5f8116c277133e772779f9d881a0a5b6d
Danny Love <feldhaus@cecs.pdx.edu>
2023-11-10 19:44:31 -0800

    Added comments to provider.py

--- src/choc_an_simulator/provider.py
+++ src/choc_an_simulator/provider.py
@@ -1,27 +1,64 @@
-"""Provider Sub-System."""
-# from .schemas import MEMBER_INFO, SERVICE_LOG_INFO, PROVIDER_DIRECTORY_INFO
+"""
+Provider Sub-System.
+
+This module facilitates interactions between healthcare providers and the ChocAn Data Center.
+It encompasses functions for displaying menus, prompting for member IDs, recording service
+billing entries, and managing provider directories. These functions collectively support
+billing, service verification, and data retrieval processes in the ChocAn system.
+"""
 
 
 def show_provider_menu() -> None:
-    """Placeholder for displaying the provider menu."""
+    """
+    Display the provider menu to the user.
+
+    Present the provider with a range of menu options, allowing access to various functionalities
+    of the Provider Component, like requesting the provider directory or recording service entries.
+    """
     pass
 
 
 def prompt_member_id() -> int:
-    """Placeholder prompting for member_id."""
+    """
+    Prompt for a valid member ID from keycard reader or terminal.
+
+    Initiates a prompt for the user to enter a member ID, which can be entered through a
+    keycard reader or manually via the terminal. The function returns the entered member ID,
+    which is used in subsequent operations like service billing or member status checks.
+
+    Returns-
+        int: The entered member ID.
+    """
     pass
 
 
 def display_member_information(member_id: int) -> None:
-    """Placeholder for displaying member information."""
+    """
+    Fetch and display information about a member from the member database.
+
+    The displayed data includes the member's name, ID, and status, aiding providers in
+    verifying member eligibility and record accuracy.
+
+    Args-
+        member_id (int): ID of the member whose information is to be displayed.
+    """
     pass
 
 
 def record_service_billing_entry(member_id: int) -> None:
-    """Placeholder for recording a service entry."""
+    """
+    Record a billing entry for a service provided to a member.
+
+    In this function, the provider enters details of the service rendered. It involves
+    validating the member's status, collecting service details, and saving the information
+    in the service logs.
+
+    Args-
+        member_id (int): The member ID for whom the service billing entry is being recorded.
+    """
     pass
 
 
 def request_provider_directory() -> None:
-    """Placeholder for requesting a provider directory."""
+    """Save the provider directory to a CSV file, and display the path it was saved to."""
     pass
