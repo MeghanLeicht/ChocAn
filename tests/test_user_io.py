19470a9981da62d2a39f1acbdb57c2e6403e69ce
Danny Love <feldhaus@cecs.pdx.edu>
2023-11-13 19:27:53 -0800

    Refactoring & commenting in user_io.py

--- tests/test_user_io.py
+++ tests/test_user_io.py
@@ -12,15 +12,16 @@ from choc_an_simulator.user_io import (
 
 
 class TestPColor:
-    """Tests of the PColor class."""
+    """Validate functionality and error handling of all functions in the PColor class."""
 
-    test_parameters = [
-        (PColor.pfail, PColor.AnsiColor.FAIL),
-        (PColor.pwarn, PColor.AnsiColor.WARNING),
-        (PColor.pok, PColor.AnsiColor.OKGREEN),
-    ]
-
-    @pytest.mark.parametrize("func,ansi_code", test_parameters)
+    @pytest.mark.parametrize(
+        "func,ansi_code",
+        [
+            (PColor.pfail, PColor.AnsiColor.FAIL),
+            (PColor.pwarn, PColor.AnsiColor.WARNING),
+            (PColor.pok, PColor.AnsiColor.OKGREEN),
+        ],
+    )
     def test_pfuncs(self, func, ansi_code, capsys):
         """Parameterized tests of the various p{color type} functions."""
         func("TEST")
@@ -32,39 +33,41 @@ class TestPColor:
 
 
 class TestPromptDate:
-    """Tests of the prompt_date function"""
+    """Validate functionality and error handling of the prompt_date function."""
 
-    test_parameters = [
-        # Normal input, leading zeros
-        (["02-01-2021"], None, None, date(2021, 2, 1)),
-        # Normal input, no leading zeros
-        (["2-1-2021"], None, None, date(2021, 2, 1)),
-        # Normal input, in range
-        (["2-1-2021"], date(2021, 1, 1), date(2021, 2, 1), date(2021, 2, 1)),
-        # Below parsing range
-        (["0-0-0000", "2-1-2021"], None, None, date(2021, 2, 1)),
-        # Above parsing range
-        (["0-0-10000", "2-1-2021"], None, None, date(2021, 2, 1)),
-        # Below min_date
-        (
-            ["1-1-2020", "2-1-2021"],
-            date(2021, 1, 1),
-            date(2022, 1, 1),
-            date(2021, 2, 1),
-        ),
-        # Above max_date
-        (
-            ["1-1-2023", "2-1-2021"],
-            date(2021, 1, 1),
-            date(2022, 1, 1),
-            date(2021, 2, 1),
-        ),
-        # Invalid
-        (["invalid", "2-1-2021"], None, None, date(2021, 2, 1)),
-    ]
-
-    @pytest.mark.parametrize("input_strs,min_date,max_date,expected", test_parameters)
+    @pytest.mark.parametrize(
+        "input_strs,min_date,max_date,expected",
+        [
+            # Normal input, leading zeros
+            (["02-01-2021"], None, None, date(2021, 2, 1)),
+            # Normal input, no leading zeros
+            (["2-1-2021"], None, None, date(2021, 2, 1)),
+            # Normal input, in range
+            (["2-1-2021"], date(2021, 1, 1), date(2021, 2, 1), date(2021, 2, 1)),
+            # Below parsing range
+            (["0-0-0000", "2-1-2021"], None, None, date(2021, 2, 1)),
+            # Above parsing range
+            (["0-0-10000", "2-1-2021"], None, None, date(2021, 2, 1)),
+            # Below min_date
+            (
+                ["1-1-2020", "2-1-2021"],
+                date(2021, 1, 1),
+                date(2022, 1, 1),
+                date(2021, 2, 1),
+            ),
+            # Above max_date
+            (
+                ["1-1-2023", "2-1-2021"],
+                date(2021, 1, 1),
+                date(2022, 1, 1),
+                date(2021, 2, 1),
+            ),
+            # Invalid
+            (["invalid", "2-1-2021"], None, None, date(2021, 2, 1)),
+        ],
+    )
     def test_prompt_date(self, input_strs, min_date, max_date, expected, monkeypatch):
+        """Test prompt_date() with a valid date entry"""
         inputs = iter(input_strs)
         monkeypatch.setattr("builtins.input", lambda _: next(inputs))
         result = prompt_date("Enter Date", min_date, max_date)
@@ -82,15 +85,16 @@ class TestPromptDate:
 
 
 class TestPromptMenuOptions:
-    """Tests of the prompt_menu_options function"""
+    """Validate functionality and error handling of the prompt_menu_options function."""
 
-    test_parameters = [
-        (["A", "B", "C"], ["1"], (0, "A")),
-        (["A", "B", "C"], ["0", "2"], (1, "B")),
-        (["A", "B", "C"], ["-1", "3"], (2, "C")),
-    ]
-
-    @pytest.mark.parametrize("choices,input_strs,expected", test_parameters)
+    @pytest.mark.parametrize(
+        "choices,input_strs,expected",
+        [
+            (["A", "B", "C"], ["1"], (0, "A")),
+            (["A", "B", "C"], ["0", "2"], (1, "B")),
+            (["A", "B", "C"], ["-1", "3"], (2, "C")),
+        ],
+    )
     def test_prompt_menu_options_valid_choice(
         self, choices, input_strs, expected, monkeypatch
     ):
@@ -112,34 +116,38 @@ class TestPromptMenuOptions:
 
 
 class TestToInt:
-    """Test of the _to_int_ function"""
+    """Validate functionality and error handling of the _to_int_ function."""
 
-    test_parameters = [
-        ("123", 123),  # Positive int
-        ("-123", -123),  # Negative int
-        ("1.23", None),  # Decimal
-        ("abc", None),  # Non-numeric text
-        ("", None),  # Empty string
-        ("@#$", None),  # Special characters
-    ]
-
-    @pytest.mark.parametrize("input,expected_output", test_parameters)
+    @pytest.mark.parametrize(
+        "input,expected_output",
+        [
+            ("123", 123),  # Positive int
+            ("-123", -123),  # Negative int
+            ("1.23", None),  # Decimal
+            ("abc", None),  # Non-numeric text
+            ("", None),  # Empty string
+            ("@#$", None),  # Special characters
+        ],
+    )
     def test_to_int_valid(self, input, expected_output):
         assert _to_int_(input) == expected_output
 
 
 class TestPromptInt:
-    test_parameters = [
-        (["1"], None, None),  # Normal input
-        (["10"], range(1, 4), range(9, 12)),  # Input in range
-        (["text", 1], None, None),  # Non-numeric before valid
-        (["-1", "10"], None, range(9, 12)),  # Below numeric range before valid
-        (["20", "10"], None, range(9, 12)),  # Above numeric before valid
-        (["1", "100"], range(2, 4), None),  # Below character limit before valid
-        (["10000", "100"], range(2, 4), None),  # Above character limit before valid
-    ]
+    """Validate functionality and error handling of the prompt_int function."""
 
-    @pytest.mark.parametrize("input_strs,char_limit,numeric_limit", test_parameters)
+    @pytest.mark.parametrize(
+        "input_strs,char_limit,numeric_limit",
+        [
+            (["1"], None, None),  # Normal input
+            (["10"], range(1, 4), range(9, 12)),  # Input in range
+            (["text", 1], None, None),  # Non-numeric before valid
+            (["-1", "10"], None, range(9, 12)),  # Below numeric range before valid
+            (["20", "10"], None, range(9, 12)),  # Above numeric before valid
+            (["1", "100"], range(2, 4), None),  # Below character limit before valid
+            (["10000", "100"], range(2, 4), None),  # Above character limit before valid
+        ],
+    )
     def test_prompt_int(self, input_strs, char_limit, numeric_limit, monkeypatch):
         """Test prompt_int using a list of parameters"""
         inputs = iter(input_strs)
@@ -154,16 +162,17 @@ class TestPromptInt:
 
 
 class TestPromptString:
-    """Tests of the prompt_string() function"""
+    """Validate functionality and error handling of the prompt_str function."""
 
-    test_parameters = [
-        (["test"], None),
-        (["test"], range(1, 4)),
-        (["long input", "test"], range(1, 4)),
-        ([""], range(0)),
-    ]
-
-    @pytest.mark.parametrize("input_strs,char_limit", test_parameters)
+    @pytest.mark.parametrize(
+        "input_strs,char_limit",
+        [
+            (["test"], None),
+            (["test"], range(1, 4)),
+            (["long input", "test"], range(1, 4)),
+            ([""], range(0)),
+        ],
+    )
     def test_prompt_string(self, input_strs, char_limit, monkeypatch):
         """Test prompt_string using a list of parameters"""
         inputs = iter(input_strs)
