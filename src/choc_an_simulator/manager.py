f269951b5d352dbbd0aaa9593678fd87d951ad7b
Kyle Joseph <kyjoseph@ada.cs.pdx.edu>
2023-11-14 20:35:30 -0800

    Wrote all the function definitions for the manager sub system.

---
+++ src/choc_an_simulator/manager.py
@@ -0,0 +1,89 @@
+""" 
+    Manager Sub-System.  
+
+    The manager sub-system of ChocAn allows managers to 
+    manage member, provider, and provider directory records. 
+    They also can generate member, provider, and summary reports.
+"""
+
+def manager_menu() -> None:
+    """
+    The Manager menu provides access to the following key functionalities:
+    Request Report
+    Update Provider
+    Update Member
+    Update Provider Directory.
+    """
+    raise NotImplementedError("manager_menu")
+
+def add_member_record() -> None:
+    """
+    Adds a member record. 
+    """
+    raise NotImplementedError("add_member_record")
+
+def update_member_record() -> None:
+    """
+    Updates a member record. 
+    """
+    raise NotImplementedError("update_member_record")
+
+def remove_member_record() -> None:
+    """
+    Removes a member record. 
+    """
+    raise NotImplementedError("remove_member_record")
+
+def add_provider_record() -> None:
+    """
+    Adds a provider record.
+    """
+    raise NotImplementedError("add_provider_record")
+
+def update_provider_record() -> None:
+    """
+    Updates a provider record. 
+    """
+    raise NotImplementedError("update_provider_record")
+
+def remove_provider_record() -> None:
+    """
+    Removes a provider record. 
+    """
+    raise NotImplementedError("remove_provider_record")
+
+def add_provider_directory_record() -> None:
+    """
+    Adds a provider directory record. 
+    """
+    raise NotImplementedError("add_provider_directory_record")
+
+def update_provider_directory_record() -> None:
+    """
+    Updates a provider directory record. 
+    """
+    raise NotImplementedError("update_provider_directory_record")
+
+def remove_provider_directory_record() -> None:
+    """
+    Removes a provider directory record. 
+    """
+    raise NotImplementedError("remove_provider_directory_record")
+
+def generate_member_report() -> None:
+    """
+    Generates a member report. 
+    """
+    raise NotImplementedError("generate_member_report")
+
+def generate_provider_report() -> None:
+    """
+    Generates a provider report.    
+    """
+    raise NotImplementedError("generate_provider_report")
+
+def generate_summary_report() -> None:
+    """
+    Generates a summary report.
+    """
+    raise NotImplementedError("generate_summary_report")
