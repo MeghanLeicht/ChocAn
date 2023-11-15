8fe00ad3b17089a5da7900782b46d90adcaeecd7
Kyle Joseph <kyjoseph@ada.cs.pdx.edu>
2023-11-15 14:56:03 -0800

    Added more detailed function comments.

--- src/choc_an_simulator/manager.py
+++ src/choc_an_simulator/manager.py
@@ -1,14 +1,16 @@
-""" 
-    Manager Sub-System.  
-
-    The manager sub-system of ChocAn allows managers to 
-    manage member, provider, and provider directory records. 
-    They also can generate member, provider, and summary reports.
 """
+Manager Sub-System.
+
+The manager sub-system of ChocAn allows managers to
+manage member, provider, and provider directory records.
+They also can generate member, provider, and summary reports.
+"""
+
 
 def manager_menu() -> None:
     """
-    The Manager menu provides access to the following key functionalities:
+    The Manager menu provides access to the following key functionalities.
+
     Request Report
     Update Provider
     Update Member
@@ -16,74 +18,166 @@ def manager_menu() -> None:
     """
     raise NotImplementedError("manager_menu")
 
+
+'''
+def add_member() -> None:
+    """Adds a member."""
+    raise NotImplementedError("add_member")
+
+
+def remove_member() -> None:
+    """Removes a member."""
+    raise NotImplementedError("remove_member")
+'''
+
+
 def add_member_record() -> None:
     """
-    Adds a member record. 
+    Manager is prompted to enter member information.
+
+    The required member information: name, street address, city, state, zip code, and email address.
+    The member number will be generated from _generate_member_id().
+
+    This prompt repeats until the user chooses to exit.
     """
     raise NotImplementedError("add_member_record")
 
+
 def update_member_record() -> None:
     """
-    Updates a member record. 
+    Prompt the user to update the member's information.
+
+    Prompts the user for a member ID, then prompts for which field to change.
+    This prompt repeats until the user chooses to exit.
     """
     raise NotImplementedError("update_member_record")
 
+
 def remove_member_record() -> None:
     """
-    Removes a member record. 
+    Prompt the user to remove the member's information.
+
+    Prompts the user for a member ID, then prompts for which field to remove.
+
+    This prompt repeats until the user chooses to exit.
     """
     raise NotImplementedError("remove_member_record")
 
+
 def add_provider_record() -> None:
     """
-    Adds a provider record.
+    Manager is prompted to enter provider information.
+
+    Provider information: name, street address, city, state, zip code, and email address.
+    Provider number is generated from _generate_provider_id.
+
+    This prompt repeats until the user chooses to exit.
     """
     raise NotImplementedError("add_provider_record")
 
+
 def update_provider_record() -> None:
     """
-    Updates a provider record. 
+    Prompt the user to update the provider's information.
+
+    Prompts the user for a provider ID, then prompts for which field to change.
+    This prompt repeats until the user chooses to exit.
     """
     raise NotImplementedError("update_provider_record")
 
+
 def remove_provider_record() -> None:
     """
-    Removes a provider record. 
+    Prompt the user to remove the provider's information.
+
+    Prompts the user for a provider ID, then prompts for which field to remove.
+    This prompt repeats until the user chooses to exit.
     """
     raise NotImplementedError("remove_provider_record")
 
+
 def add_provider_directory_record() -> None:
     """
-    Adds a provider directory record. 
+    Manager is prompted to enter service information.
+
+    This is the service information: service_id, service_name, price_dollars, and price_cents.
+
+    This prompt repeats until the user chooses to exit.
     """
     raise NotImplementedError("add_provider_directory_record")
 
+
 def update_provider_directory_record() -> None:
     """
-    Updates a provider directory record. 
+    The manager is prompted for a service id to update, and a lookup is performed.
+
+    This prompt repeats until the user chooses to exit.
     """
     raise NotImplementedError("update_provider_directory_record")
 
+
 def remove_provider_directory_record() -> None:
     """
-    Removes a provider directory record. 
+    Manager is prompted for a service id to be removed, and a lookup is performed.
+
+    This prompt repeats until the user chooses to exit.
     """
     raise NotImplementedError("remove_provider_directory_record")
 
+
 def generate_member_report() -> None:
     """
-    Generates a member report. 
+    Each member who has consulted a ChocAn provider during that week receives a list of services.
+
+    This information will be sorted in order of service date.
+
+    The report, which is also sent as an e-mail attachment, includes:
+    Member name (25 characters).
+    Member number (9 digits).
+    Member street address (25 characters).
+    Member city (14 characters).
+    Member state (2 letters).
+    Member zip code (5 digits).
+    For each service provided, the following details are required:
+    Date of service (MM-DD-YYYY).
+    Provider name (25 characters).
+    Service name (20 characters).
     """
     raise NotImplementedError("generate_member_report")
 
+
 def generate_provider_report() -> None:
     """
-    Generates a provider report.    
+    Each provider who has billed ChocAn during that week receives a report.
+
+    The fields of the report include:
+    Provider name (25 characters).
+    Provider number (9 digits).
+    Provider street address (25 characters).
+    Provider city (14 characters).
+    Provider state (2 letters).
+    Provider zip code (5 digits).
+    For each service provided, the following details are required:
+    Date of service (MM-DD-YYYY).
+    Date and time data were received by the computer (MM-DD-YYYY
+    HH:MM:SS).
+    Member name (25 characters).
+    Member number (9 digits).
+    Service code (6 digits).
+    Fee to be paid (up to $999.99).
+    Total number of consultations with members (3 digits).
+    Total fee for the week (up to $99,999.99)
     """
     raise NotImplementedError("generate_provider_report")
 
+
 def generate_summary_report() -> None:
     """
-    Generates a summary report.
+    A summary report is given to the manager for accounts payable.
+
+    The report lists every provider to be paid that week, the number of consultations each had, and
+    his or her total fee for that week. Finally the total number of providers who
+    provided services, the total number of consultations, and the overall fee total are
+    printed.
     """
     raise NotImplementedError("generate_summary_report")
