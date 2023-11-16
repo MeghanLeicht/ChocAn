b4cfea9a65445e88fe0ed2d2578f1e7d17c0d79f
Danny Love <feldhaus@cecs.pdx.edu>
2023-11-15 19:14:48 -0800

    Implement provider menu

--- src/choc_an_simulator/provider.py
+++ src/choc_an_simulator/provider.py
@@ -6,6 +6,7 @@ It encompasses functions for displaying menus, prompting for member IDs, recordi
 billing entries, and managing provider directories. These functions collectively support
 billing, service verification, and data retrieval processes in the ChocAn system.
 """
+from choc_an_simulator.user_io import prompt_menu_options
 
 
 def show_provider_menu() -> None:
@@ -15,7 +16,28 @@ def show_provider_menu() -> None:
     Present the provider with a range of menu options, allowing access to various functionalities
     of the Provider Component, like requesting the provider directory or recording service entries.
     """
+    match prompt_menu_options(
+        "Provider Menu",
+        ["Request Provider Directory", "Record a Service", "Member Check-In"],
+    ):
+        case (_, "Request Provider Directory"):
+            request_provider_directory()
+        case (_, "Record a Service"):
+            record_service_billing_entry()
+        case (_, "Member Check-In"):
+            check_in_member()
     pass
+
+
+def check_in_member() -> None:
+    """
+    Prompt for a member's ID and display their status.
+
+    Initiates a prompt for the user to enter a member ID, which can be entered through a
+    keycard reader or manually via the terminal. Then, displays either "Valid", "Suspended"
+    or "Invalid"
+    """
+    raise NotImplementedError("check_in_member")
 
 
 def prompt_member_id() -> int:
@@ -45,16 +67,13 @@ def display_member_information(member_id: int) -> None:
     pass
 
 
-def record_service_billing_entry(member_id: int) -> None:
+def record_service_billing_entry() -> None:
     """
     Record a billing entry for a service provided to a member.
 
     In this function, the provider enters details of the service rendered. It involves
     validating the member's status, collecting service details, and saving the information
     in the service logs.
-
-    Args-
-        member_id (int): The member ID for whom the service billing entry is being recorded.
     """
     pass
 
