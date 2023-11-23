c8bc503d23b4fbacef8098f05e1be03bb4c9269f
Vlad Chevdar <chevdar@cecs.pdx.edu>
2023-11-22 16:57:50 -0800

    Implement record_service_billing_entry function

--- src/choc_an_simulator/provider.py
+++ src/choc_an_simulator/provider.py
@@ -47,17 +47,45 @@ def display_member_information(member_id: int) -> None:
 
 def record_service_billing_entry(member_id: int) -> None:
     """
-    Record a billing entry for a service provided to a member.
+    Records a service billing entry for a ChocAn member.
 
-    In this function, the provider enters details of the service rendered. It involves
-    validating the member's status, collecting service details, and saving the information
-    in the service logs.
-
-    Args-
-        member_id (int): The member ID for whom the service billing entry is being recorded.
+    Args:
+        member_id (int): The member's unique nine-digit ID.
     """
-    pass
+    # Spep 1: Authenticate Member
+    if not validate_member(member_id):
+        display_error("Invalid Member ID or Member Suspended")
+        return
 
+    # Step 2: Enter Service Date
+    service_date = input_service_date()  # Format: MM-DD-YYYY
+
+    # Step 3: Enter Service Code
+    service_code = input_service_code()
+    if not validate_service_code(service_code):
+        display_error("Invalid Service Code")
+        return
+
+    # Optionally display the service name for verification
+    service_name = get_service_name(service_code)
+    if not confirm_service(service_name):
+        return
+
+    # Step 4: Optional Comments
+    comments = input_comments()  # Up to 100 characters
+
+    # Step 5: Create Record
+    current_datetime = get_current_datetime()  # Format: MM-DD-YYYY HH:MM:SS
+    provider_id = get_current_provider_id()  # The ID of the provider using the system
+    record = create_service_record(current_datetime, service_date, provider_id, member_id, service_code, comments)
+
+    # Step 6: Display Fee and Record for Verification
+    fee = get_service_fee(service_code)
+    display_fee(fee)
+    record_fee_for_verification(record)
+
+    # Step 7: Save Record for Reporting
+    save_record(record)
 
 def request_provider_directory() -> None:
     """Save the provider directory to a CSV file, and display the path it was saved to."""
