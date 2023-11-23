28043950f5ef283626717e24ea810298fd090393
Meghan Leicht <112194403+MeghanLeicht@users.noreply.github.com>
2023-11-22 18:28:53 -0800

    forgot the generate report functions

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -13,22 +13,29 @@ from choc_an_simulator.manager import (
     remove_provider_directory_record,
     generate_member_report,
     generate_provider_report,
-    generate_summary_report
+    generate_summary_report,
 )
+
 
 @pytest.mark.parametrize(
     "menu_option",
-    [ ("Member"), ("Provider"), ("Provider Directory"), ("Reports"),],
+    [
+        ("Member"),
+        ("Provider"),
+        ("Provider Directory"),
+        ("Reports"),
+    ],
 )
 def test_manager_menu(menu_option):
     if menu_option == "Member":
+
         @pytest.mark.parametrize(
-        "option_text,endpoint_func_name",
-        [
-            ("Add", "choc_an_simulator.manager.add_member_record"),
-            ("Update", "choc_an_simulator.manager.update_member_record"),
-            ("Remove", "choc_an_simulator.manager.remove_member_record"),
-        ]
+            "option_text,endpoint_func_name",
+            [
+                ("Add", "choc_an_simulator.manager.add_member_record"),
+                ("Update", "choc_an_simulator.manager.update_member_record"),
+                ("Remove", "choc_an_simulator.manager.remove_member_record"),
+            ],
         )
         @pytest.mark.usefixtures("assert_menu_endpoint")
         def test_member_option(
@@ -37,14 +44,16 @@ def test_manager_menu(menu_option):
             option_text: str,
         ):
             manager_menu()
+
     elif menu_option == "Provider":
+
         @pytest.mark.parametrize(
-        "option_text,endpoint_func_name",
-        [
-            ("Add", "choc_an_simulator.manager.add_provider_record"),
-            ("Update", "choc_an_simulator.manager.update_provider_record"),
-            ("Remove", "choc_an_simulator.manager.remove_provider_record"),
-        ]
+            "option_text,endpoint_func_name",
+            [
+                ("Add", "choc_an_simulator.manager.add_provider_record"),
+                ("Update", "choc_an_simulator.manager.update_provider_record"),
+                ("Remove", "choc_an_simulator.manager.remove_provider_record"),
+            ],
         )
         @pytest.mark.usefixtures("assert_menu_endpoint")
         def test_provider_option(
@@ -53,14 +62,22 @@ def test_manager_menu(menu_option):
             option_text: str,
         ):
             manager_menu()
+
     elif menu_option == "Provider Directory":
+
         @pytest.mark.parametrize(
-        "option_text,endpoint_func_name",
-        [
-            ("Add", "choc_an_simulator.manager.add_provider_directory_record"),
-            ("Update", "choc_an_simulator.manager.update_provider_directory_record"),
-            ("Remove", "choc_an_simulator.manager.remove_provider_directory_record"),
-        ]
+            "option_text,endpoint_func_name",
+            [
+                ("Add", "choc_an_simulator.manager.add_provider_directory_record"),
+                (
+                    "Update",
+                    "choc_an_simulator.manager.update_provider_directory_record",
+                ),
+                (
+                    "Remove",
+                    "choc_an_simulator.manager.remove_provider_directory_record",
+                ),
+            ],
         )
         @pytest.mark.usefixtures("assert_menu_endpoint")
         def test_provider_directory_option(
@@ -69,14 +86,16 @@ def test_manager_menu(menu_option):
             option_text: str,
         ):
             manager_menu()
+
     elif menu_option == "Reports":
+
         @pytest.mark.parametrize(
-        "option_text,endpoint_func_name",
-        [
-            ("Member", "choc_an_simulator.manager.generate_member_report"),
-            ("Provider", "choc_an_simulator.manager.generate_provider_report"),
-            ("Summary", "choc_an_simulator.manager.generate_summary_report"),
-        ]
+            "option_text,endpoint_func_name",
+            [
+                ("Member", "choc_an_simulator.manager.generate_member_report"),
+                ("Provider", "choc_an_simulator.manager.generate_provider_report"),
+                ("Summary", "choc_an_simulator.manager.generate_summary_report"),
+            ],
         )
         @pytest.mark.usefixtures("assert_menu_endpoint")
         def test_reports_option(
@@ -130,3 +149,18 @@ def test_update_provider_directory_record():
 def test_remove_provider_directory_record():
     with pytest.raises(NotImplementedError):
         remove_provider_directory_record()
+
+
+def test_generate_member_report():
+    with pytest.raises(NotImplementedError):
+        generate_member_report()
+
+
+def test_generate_provider_report():
+    with pytest.raises(NotImplementedError):
+        generate_provider_report()
+
+
+def test_generate_summary_report():
+    with pytest.raises(NotImplementedError):
+        generate_summary_report()
