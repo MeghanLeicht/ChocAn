f8e329eba7156aadd3cf866802f85c2548ca935b
Vlad Chevdar <chevdar@cecs.pdx.edu>
2023-11-22 17:07:44 -0800

    Implement record_service_billing_entry function

--- src/choc_an_simulator/provider.py
+++ src/choc_an_simulator/provider.py
@@ -57,34 +57,34 @@ def record_service_billing_entry(member_id: int) -> None:
         display_error("Invalid Member ID or Member Suspended")
         return
 
-    # Step 2: Enter Service Date
+    # Step 2: Get Service Date
     service_date = input_service_date()  # Format: MM-DD-YYYY
 
-    # Step 3: Enter Service Code
+    # Step 3: Get Service Code
     service_code = input_service_code()
     if not validate_service_code(service_code):
         display_error("Invalid Service Code")
         return
 
-    # Optionally display the service name for verification
+    # Step 4: Display the service name for verification
     service_name = get_service_name(service_code)
     if not confirm_service(service_name):
         return
 
-    # Step 4: Optional Comments
+    # Step 5: Get Optional Comments
     comments = input_comments()  # Up to 100 characters
 
-    # Step 5: Create Record
+    # Step 6: Create Record
     current_datetime = get_current_datetime()  # Format: MM-DD-YYYY HH:MM:SS
     provider_id = get_current_provider_id()  # The ID of the provider using the system
     record = create_service_record(current_datetime, service_date, provider_id, member_id, service_code, comments)
 
-    # Step 6: Display Fee and Record for Verification
+    # Step 7: Display Fee and Record for Verification
     fee = get_service_fee(service_code)
     display_fee(fee)
     record_fee_for_verification(record)
 
-    # Step 7: Save Record for Reporting
+    # Step 8: Save Record for Reporting
     save_record(record)
 
 def request_provider_directory() -> None:
