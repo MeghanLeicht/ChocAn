5b83b44817cc9fdd0ff62cda6b872da00aa7df6b
Danny Love <feldhaus@cecs.pdx.edu>
2023-11-25 11:47:18 -0800

    Minor test corrections

--- tests/test_provider.py
+++ tests/test_provider.py
@@ -8,23 +8,20 @@ from choc_an_simulator.provider import (
     request_provider_directory,
 )
 
+CAS_PVDR_PATH = "choc_an_simulator.provider"
+
 
 @pytest.mark.parametrize(
     "option_text,endpoint_func_name",
     [
-        (
-            "Request Provider Directory",
-            "choc_an_simulator.provider.request_provider_directory",
-        ),
-        ("Record a Service", "choc_an_simulator.provider.record_service_billing_entry"),
-        ("Member Check-In", "choc_an_simulator.provider.check_in_member"),
+        ("Request Provider Directory", f"{CAS_PVDR_PATH}.request_provider_directory"),
+        ("Record a Service", f"{CAS_PVDR_PATH}.record_service_billing_entry"),
+        ("Member Check-In", f"{CAS_PVDR_PATH}.check_in_member"),
     ],
 )
 @pytest.mark.usefixtures("assert_menu_endpoint")
 def test_show_provider_menu(
     assert_menu_endpoint,
-    endpoint_func_name: str,
-    option_text: str,
 ):
     """Paramaterized test that show_provider_menu reaches the correct endpoints"""
     show_provider_menu()
