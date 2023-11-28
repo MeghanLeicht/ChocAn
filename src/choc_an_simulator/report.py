d44c0d22f884ccf40e7fc155d4f809738119ab4a
Danny Feldhaus <danny.feldhaus@pgn.com>
2023-11-28 15:09:17 -0800

    Made suggested changes

--- src/choc_an_simulator/report.py
+++ src/choc_an_simulator/report.py
@@ -3,56 +3,21 @@ Functions for generating reports.
 """
 
 from datetime import datetime, timedelta
-from typing import Any, Dict, List, Optional
 
 import pandas as pd
 import pyarrow as pa
-from pandas.errors import MergeError
 
 from choc_an_simulator.database_management.load_records import load_records_from_file
 from choc_an_simulator.database_management.reports import save_report
 from choc_an_simulator.user_io import PColor
-from choc_an_simulator.schemas import MEMBER_INFO, PROVIDER_DIRECTORY_INFO, SERVICE_LOG_INFO, TableInfo, USER_INFO
+from choc_an_simulator.schemas import (
+    MEMBER_INFO,
+    PROVIDER_DIRECTORY_INFO,
+    SERVICE_LOG_INFO,
+    USER_INFO,
+)
 
 
-def error_handler(func):
-    """
-    Decorator to handle exceptions raised by calls to 'database_management.py'
-    and the pandas library.
-    """
-
-    def wrapper(*args, **kwargs):
-        """
-        Wrapper function to handle exceptions raised by calls to 'database_management.py'.
-        """
-        try:
-            func(*args, **kwargs)
-        except KeyError as err_key:
-            PColor.pfail(f"KeyError: {err_key}")
-            return
-        except pa.ArrowTypeError as err_type:
-            PColor.pfail(f"ArrowTypeError: {err_type}")
-            return
-        except pa.ArrowInvalid as err_invalid:
-            PColor.pfail(f"ArrowInvalid: {err_invalid}")
-            return
-        except pa.ArrowIOError as err_io:
-            PColor.pfail(f"ArrowIOError: {err_io}")
-            return
-        except TypeError as err_type:
-            PColor.pfail(f"TypeError: {err_type}")
-            return
-        except MergeError as err_merge:
-            PColor.pfail(f"MergeError: {err_merge}")
-            return
-        except ValueError as err_value:
-            PColor.pfail(f"ValueError: {err_value}")
-            return
-
-    return wrapper
-
-
-@error_handler
 def generate_member_report() -> None:
     """
     Generate a report for each member who has consulted with a ChocAn provider.
@@ -72,165 +37,40 @@ def generate_member_report() -> None:
         TypeError: If a column value is not of the correct type.
     """
     gt_cols = {"service_date_utc": datetime.now() - timedelta(days=7)}
-    tables = [
-        _generate_data_dict(SERVICE_LOG_INFO, gt_cols=gt_cols,
-                            kept_cols=['service_date_utc', 'member_id', 'provider_id', 'service_id']),
-        _generate_data_dict(MEMBER_INFO, kept_cols=['member_id', 'name', 'address', 'city', 'state', 'zipcode']),
-        _generate_data_dict(USER_INFO, kept_cols=['name', 'id']),
-        _generate_data_dict(PROVIDER_DIRECTORY_INFO, kept_cols=['service_name', 'service_id']),
-    ]
-
-    records = _load_all_records(tables)
-
-    if records['service_log'].empty:
-        print("No records found within the last 7 days.")
+    service_log_cols = ["service_date_utc", "member_id", "provider_id", "service_id"]
+    member_cols = ["member_id", "name", "address", "city", "state", "zipcode"]
+    user_cols = ["name", "id"]
+    provider_dir_cols = ["service_name", "service_id"]
+    try:
+        service_log = load_records_from_file(SERVICE_LOG_INFO, gt_cols=gt_cols)[service_log_cols]
+        if service_log.empty:  # Return immediately if empty - no need to load the other data.
+            print("No records found within the last 7 days.")
+            return
+        member_info = load_records_from_file(MEMBER_INFO)[member_cols]
+        user_info = load_records_from_file(USER_INFO)[user_cols]
+        provder_directory = load_records_from_file(PROVIDER_DIRECTORY_INFO)[provider_dir_cols]
+    except pa.ArrowIOError as err_io:
+        PColor.pwarn(f"There was an issue accessing the database.\n\tError: {err_io}")
         return
 
-    merge_instructions = [
-        {
-            'left_df': records[SERVICE_LOG_INFO.name],
-            'right_df': records[PROVIDER_DIRECTORY_INFO.name],
-            'merge_kwargs': {
-                'on': 'service_id'
-            },
-        },
-        {
-            'right_df': records[USER_INFO.name],
-            'merge_kwargs': {
-                'left_on': 'provider_id',
-                'right_on': 'id'
-            },
-            'drop_cols': ['id'],
-        },
-        {
-            'right_df': records[MEMBER_INFO.name],
-            'merge_kwargs': {
-                'on': 'member_id'
-            },
-        },
-    ]
+    records = pd.merge(service_log, provder_directory, on="service_id")
+    records = pd.merge(records, user_info, left_on="provider_id", right_on="id")
+    records = pd.merge(records, member_info, on="member_id")
+    records = records.drop(columns="id")
 
-    records = merge_dataframes(merge_instructions)
-
-    records = records.groupby('member_id').apply(
-        lambda x: pd.DataFrame(
-            {
-                'Member name': x['name_y'].iloc[0],
-                'Member number': x['member_id'].iloc[0],
-                'Member street address': x['address'].iloc[0],
-                'Member city': x['city'].iloc[0],
-                'Member state': x['state'].iloc[0],
-                'Member zip code': x['zipcode'].iloc[0],
-                'Services': [
-                    sorted(
-                        x[['service_date_utc', 'name_x', 'service_name']].values.tolist(),
-                        key=lambda y: pd.to_datetime(y[0])
-                    )
-                ]
-            }
-        )
-    ).reset_index(drop=True)
-
-    list_of_members = [pd.DataFrame(records.iloc[i]).T for i in range(len(records))]
-
-    for member_record in list_of_members:
-        file_path = save_report(member_record, f"{member_record['Member name'].values.item()}_{_current_date()}")
+    records = records.rename(
+        columns={
+            "name_y": "Name",
+            "member_id": "Member Number",
+            "name_x": "Provider Name",
+            "service_date_utc": "Service Date (Local Time)",
+        }
+    )
+    for member_id in records["Member Number"].unique():
+        member_records = records[records["Member Number"] == member_id]
+        member_name = member_records["Name"].iloc[0]
+        file_path = save_report(member_records, f"{member_name}_{_current_date()}")
         print(f"Report saved to {file_path}")
-
-
-def _load_all_records(tables: List[Dict[str, Any]]) -> Dict[str, pd.DataFrame]:
-    """
-    Load the data needed to generate the member report.
-
-    Returns-
-        A dataframe containing the data needed to generate the member report.
-
-    Raises-
-        KeyError: If a column name is not found in the schema.
-        ArrowTypeError: If a column value is not of the correct type.
-        ArrowInvalid: If a column value is invalid.
-        ArrowIOError: If an IO error occurs.
-        TypeError: If a column value is not of the correct type.
-    """
-    records = {}
-    for table in tables:
-        records[table['table'].name] = load_records_from_file(
-            table['table'],
-            gt_cols=table['filter'].get('gt_cols'),
-            lt_cols=table['filter'].get('lt_cols'),
-            eq_cols=table['filter'].get('eq_cols'),
-        )[table['kept_cols']]
-
-    return records
-
-
-def merge_dataframes(merge_instructions: List[Dict[str, Any]]) -> pd.DataFrame:
-    """
-    Merge dataframes.
-
-    Args-
-        df_dict (Dict[str, pd.DataFrame]): A dictionary of dataframes to merge.
-        merge_instructions (List[Dict[str, Any]]): list of dictionaries containing instructions for merging dataframes.
-
-    Returns-
-        A merged dataframe.
-
-    Raises-
-        KeyError: If a column name is not found in the schema.
-        ArrowTypeError: If a column value is not of the correct type.
-        ArrowInvalid: If a column value is invalid.
-        ArrowIOError: If an IO error occurs.
-        TypeError: If a column value is not of the correct type.
-    """
-    merged_dfs = pd.DataFrame()
-    for instruction in merge_instructions:
-        merged_dfs = pd.merge(
-            instruction['left_df'] if merged_dfs.empty else merged_dfs,
-            instruction['right_df'],
-            **instruction['merge_kwargs']
-        )
-        if 'drop_cols' in instruction:
-            merged_dfs = merged_dfs.drop(instruction['drop_cols'], axis=1)
-
-    return merged_dfs
-
-
-def _generate_data_dict(
-        table_info: TableInfo,
-        kept_cols: [List[str]],
-        eq_cols: Optional[Dict[str, Any]] = None,
-        lt_cols: Optional[Dict[str, Any]] = None,
-        gt_cols: Optional[Dict[str, Any]] = None,
-
-) -> Dict[str, Any]:
-    """
-    Generate a dictionary of TableInfo and filter information to be used to generate a report.
-
-    Args-
-        table_info (TableInfo): Object with schema and table details.
-        eq_cols (Optional[Dict[str, Any]]): Specifies columns and values for equality filtering.
-        lt_cols (Optional[Dict[str, Any]]): Specifies columns and values for less-than filtering.
-        gt_cols (Optional[Dict[str, Any]]): Specifies columns and values for greater-than filtering.
-
-    Returns-
-        A dictionary of TableInfo and filter information to be used to generate a report.
-    """
-    data_dict = {}
-    filters = {}
-    if eq_cols is not None:
-        filters['eq_cols'] = eq_cols
-    if lt_cols is not None:
-        filters['lt_cols'] = lt_cols
-    if gt_cols is not None:
-        filters['gt_cols'] = gt_cols
-
-    data_dict = {
-        'table': table_info,
-        'filter': filters,
-        'kept_cols': kept_cols,
-    }
-
-    return data_dict
 
 
 def _current_date() -> str:
--- tests/test_report.py
+++ tests/test_report.py
@@ -1,16 +1,12 @@
-import random
 from datetime import datetime
 from unittest.mock import patch
-from unittest import mock
 
 import pandas as pd
-import pyarrow
 import pyarrow as pa
 import pytest
-from pandas.errors import MergeError
 
-from choc_an_simulator.report import _generate_data_dict, generate_member_report
-from choc_an_simulator.schemas import SERVICE_LOG_INFO, TableInfo
+from choc_an_simulator.report import generate_member_report
+from choc_an_simulator.schemas import TableInfo
 
 """
 Create test data for the generate_member_report function.
@@ -19,21 +15,51 @@ test_user_info = pd.DataFrame(
     {
         "id": [940672921, 265608022, 637066975, 483185890, 385685178, 807527890],
         # "type": random.sample(0, 1), 6),
-        "name": ["Case Hall", "Regina George", "Ray Donald", "Karla Tanners", "Zelda Hammersmith", "Linda Smith"],
-        "address": ["123 Main St", "456 Elm St", "789 Oak St", "1011 Pine St", "1213 Maple St", "1415 Cedar St"],
+        "name": [
+            "Case Hall",
+            "Regina George",
+            "Ray Donald",
+            "Karla Tanners",
+            "Zelda Hammersmith",
+            "Linda Smith",
+        ],
+        "address": [
+            "123 Main St",
+            "456 Elm St",
+            "789 Oak St",
+            "1011 Pine St",
+            "1213 Maple St",
+            "1415 Cedar St",
+        ],
         "city": ["Chicago", "New York", "Los Angeles", "Portland", "Sandy", "Muscle Shoals"],
         "state": ["IL", "NY", "CA", "OR", "OR", "AL"],
         "zipcode": [15603, 38322, 84524, 64198, 34268, 73952],
-        "password_hash": pa.array(["password1", "password2", "password3", "password4", "password5", "password6"],
-                                  type=pa.binary()),
+        "password_hash": pa.array(
+            ["password1", "password2", "password3", "password4", "password5", "password6"],
+            type=pa.binary(),
+        ),
     }
 )
 
 test_member_info = pd.DataFrame(
     {
         "member_id": [137002632, 989635272, 752880910, 367868907, 344690896, 406072422],
-        "name": ["John Doe", "Jane Doe", "Alex Smith", "Bob Henderson", "Rebecca Miller", "Leah Jones"],
-        "address": ["123 Main St", "456 Elm St", "789 Oak St", "1011 Pine St", "1213 Maple St", "1415 Cedar St"],
+        "name": [
+            "John Doe",
+            "Jane Doe",
+            "Alex Smith",
+            "Bob Henderson",
+            "Rebecca Miller",
+            "Leah Jones",
+        ],
+        "address": [
+            "123 Main St",
+            "456 Elm St",
+            "789 Oak St",
+            "1011 Pine St",
+            "1213 Maple St",
+            "1415 Cedar St",
+        ],
         "city": ["Chicago", "New York", "Los Angeles", "Portland", "Sandy", "Muscle Shoals"],
         "state": ["IL", "NY", "CA", "OR", "OR", "AL"],
         "zipcode": [15603, 38322, 84524, 64198, 34268, 73952],
@@ -44,23 +70,49 @@ test_member_info = pd.DataFrame(
 
 test_service_log_info = pd.DataFrame(
     {
-        "entry_datetime_utc": [datetime(2023, 11, 21), datetime(2023, 11, 24), datetime(2023, 11, 26),
-                               datetime(2023, 11, 21), datetime(2023, 11, 21), datetime(2023, 11, 24)],
-        "service_date_utc": [datetime(2023, 11, 21), datetime(2023, 11, 24), datetime(2023, 11, 26),
-                             datetime(2023, 11, 21), datetime(2023, 11, 21), datetime(2023, 11, 24)],
+        "entry_datetime_utc": [
+            datetime(2023, 11, 21),
+            datetime(2023, 11, 24),
+            datetime(2023, 11, 26),
+            datetime(2023, 11, 21),
+            datetime(2023, 11, 21),
+            datetime(2023, 11, 24),
+        ],
+        "service_date_utc": [
+            datetime(2023, 11, 21),
+            datetime(2023, 11, 24),
+            datetime(2023, 11, 26),
+            datetime(2023, 11, 21),
+            datetime(2023, 11, 21),
+            datetime(2023, 11, 24),
+        ],
         "member_id": [367868907, 752880910, 367868907, 989635272, 752880910, 137002632],
         "provider_id": [483185890, 483185890, 940672921, 385685178, 385685178, 637066975],
         "service_id": [889804, 951175, 495644, 805554, 427757, 708195],
-        "comments": ["", "Test comment", "Test comment 2", "Test comment 3", "Test comment 4", "Test comment 5"],
+        "comments": [
+            "",
+            "Test comment",
+            "Test comment 2",
+            "Test comment 3",
+            "Test comment 4",
+            "Test comment 5",
+        ],
     }
 )
 
 test_provider_directory_info = pd.DataFrame(
     {
         "service_id": [889804, 951175, 495644, 805554, 427757, 708195],
-        "service_name": ["Service 1", "Service 2", "Service 3", "Service 4", "Service 5", "Service 6"],
-        "price_cents": random.sample(range(0, 99), 6),
-        "price_dollars": random.sample(range(1, 999), 6),
+        "service_name": [
+            "Service 1",
+            "Service 2",
+            "Service 3",
+            "Service 4",
+            "Service 5",
+            "Service 6",
+        ],
+        "price_cents": [1, 2, 3, 4, 5, 6],
+        "price_dollars": [10, 20, 30, 40, 50, 60],
     }
 )
 
@@ -103,156 +155,66 @@ def save_report_side_effect(*args, **kwargs):
 
 
 @patch("choc_an_simulator.report.save_report", side_effect=save_report_side_effect)
-@patch("choc_an_simulator.report.load_records_from_file", side_effect=load_records_from_file_side_effect)
-def test_generate_member_report(mock_load_records_from_file, mock_save_report, capfd):
+@patch(
+    "choc_an_simulator.report.load_records_from_file",
+    side_effect=load_records_from_file_side_effect,
+)
+def test_generate_member_report(mock_load_records_from_file, mock_save_report, capsys):
     """
     Test the generate_member_report function.
     """
     current_date = datetime.now().strftime("%m-%d-%Y")
-    expected_output = ''.join([
-        f"Report saved to /path/to/report/John Doe_{current_date}.csv\n",
-        f"Report saved to /path/to/report/Bob Henderson_{current_date}.csv\n",
-        f"Report saved to /path/to/report/Alex Smith_{current_date}.csv\n",
-        f"Report saved to /path/to/report/Jane Doe_{current_date}.csv\n",
-    ])
+    expected_output = "".join(
+        [
+            f"Report saved to /path/to/report/Bob Henderson_{current_date}.csv\n",
+            f"Report saved to /path/to/report/Alex Smith_{current_date}.csv\n",
+            f"Report saved to /path/to/report/Jane Doe_{current_date}.csv\n",
+            f"Report saved to /path/to/report/John Doe_{current_date}.csv\n",
+        ]
+    )
     expected_keys = [
-        "Member name",
-        "Member number",
-        "Member street address",
-        "Member city",
-        "Member state",
-        "Member zip code",
-        "Services",
+        "Name",
+        "Member Number",
+        "address",
+        "city",
+        "state",
+        "zipcode",
+        "service_id",
+        "Provider Name",
+        "provider_id",
+        "Service Date (Local Time)",
+        "service_name",
     ]
-    mock_df = mock.Mock()
 
     generate_member_report()
-    captured = capfd.readouterr()
-
-    assert (captured.out == expected_output)
-
+    captured = capsys.readouterr()
+    assert captured.out == expected_output, captured.out + " -> " + expected_output
     actual_df = mock_save_report.call_args_list[0][0][0]
     assert set(actual_df.columns) == set(expected_keys)
 
 
-@patch("choc_an_simulator.report.load_records_from_file", side_effect=load_records_from_file_side_effect)
-@patch("choc_an_simulator.report._load_all_records", return_value={'service_log': pd.DataFrame()})
-def test_generate_member_report_no_members(mock_load_records_from_file, mock_load_all_records, capfd):
+@patch(
+    "choc_an_simulator.report.load_records_from_file",
+    return_value=pd.DataFrame(
+        {"service_date_utc": [], "member_id": [], "provider_id": [], "service_id": []}
+    ),
+)
+def test_generate_member_report_no_members(mock_load_records_from_file, capsys):
     """
     Test the generate_member_report function with no members.
     """
     expected_output = "No records found within the last 7 days.\n"
-
     generate_member_report()
-    captured = capfd.readouterr()
-
-    assert (captured.out == expected_output)
-
-
-@patch("choc_an_simulator.report.load_records_from_file", side_effect=KeyError)
-def test_generate_member_report_key_error(mock_load_records_from_file, capfd):
-    """
-    Test the generate_member_report function with a KeyError.
-    """
-    expected = '\x1b[91mKeyError: \x1b[0m\n'
-    generate_member_report()
-    captured = capfd.readouterr()
-    assert (captured.out == expected)
+    captured = capsys.readouterr()
+    assert captured.out == expected_output
 
 
-@patch("choc_an_simulator.report.load_records_from_file", side_effect=pyarrow.ArrowTypeError)
-def test_generate_member_report_arrow_type_error(mock_load_records_from_file, capfd):
-    """
-    Test the generate_member_report function with a ArrowTypeError.
-    """
-    expected = '\x1b[91mArrowTypeError: \x1b[0m\n'
-    generate_member_report()
-    captured = capfd.readouterr()
-    assert (captured.out == expected)
-
-
-@patch("choc_an_simulator.report.load_records_from_file", side_effect=pyarrow.ArrowInvalid)
-def test_generate_member_report_arrow_invalid(mock_load_records_from_file, capfd):
-    """
-    Test the generate_member_report function with a ArrowInvalid.
-    """
-    expected = '\x1b[91mArrowInvalid: \x1b[0m\n'
-    generate_member_report()
-    captured = capfd.readouterr()
-    assert (captured.out == expected)
-
-
-@patch("choc_an_simulator.report.load_records_from_file", side_effect=pyarrow.ArrowIOError)
+@patch("choc_an_simulator.report.load_records_from_file", side_effect=pa.ArrowIOError)
 def test_generate_member_report_arrow_io_error(mock_load_records_from_file, capfd):
     """
     Test the generate_member_report function with a ArrowIOError.
     """
-    expected = '\x1b[91mArrowIOError: \x1b[0m\n'
+    expected = "There was an issue accessing the database."
     generate_member_report()
     captured = capfd.readouterr()
-    assert (captured.out == expected)
-
-
-@patch("choc_an_simulator.report.load_records_from_file", side_effect=TypeError)
-def test_generate_member_report_type_error(mock_load_records_from_file, capfd):
-    """
-    Test the generate_member_report function with a TypeError.
-    """
-    expected = '\x1b[91mTypeError: \x1b[0m\n'
-    generate_member_report()
-    captured = capfd.readouterr()
-    assert (captured.out == expected)
-
-
-@patch("choc_an_simulator.report.pd.merge", side_effect=MergeError)
-@patch("choc_an_simulator.report.load_records_from_file", side_effect=load_records_from_file_side_effect)
-def test_generate_member_report_merge_error(mock_load_records_from_file, mock_merge, capfd):
-    """
-    Test the generate_member_report function with a MergeError.
-    """
-    expected = '\x1b[91mMergeError: \x1b[0m\n'
-    generate_member_report()
-    captured = capfd.readouterr()
-    assert (captured.out == expected)
-
-
-@patch("choc_an_simulator.report.load_records_from_file", side_effect=load_records_from_file_side_effect)
-@patch("choc_an_simulator.report.pd.merge", side_effect=ValueError)
-def test_generate_member_report_value_error(mock_load_records_from_file, mock_merge, capfd):
-    """
-    Test the generate_member_report function with a ValueError.
-    """
-    expected = '\x1b[91mValueError: \x1b[0m\n'
-    generate_member_report()
-    captured = capfd.readouterr()
-    assert (captured.out == expected)
-
-
-kept_cols = ["member_id", "name", "address", "city", "state", "zipcode", "suspended"]
-table_info = SERVICE_LOG_INFO
-
-FILTERS = [
-    ('eq_cols', {"member_id": 137002632}),
-    ('lt_cols', {"member_id": 137002632}),
-    ('gt_cols', {"member_id": 137002632}),
-    (None, None)
-]
-
-
-@pytest.mark.parametrize('filter_type, filter_value', FILTERS)
-def test_generate_data_dict(filter_type, filter_value):
-    """
-    Parameterized test for the _generate_data_dict function.
-    """
-    params = {'kept_cols': kept_cols}
-
-    if filter_type:
-        params[filter_type] = filter_value
-
-    result = _generate_data_dict(table_info, **params)
-
-    assert result['table'] == table_info
-    assert result['kept_cols'] == kept_cols
-
-    if filter_type:
-        assert result['filter'][filter_type] == filter_value
+    assert expected in captured.out
