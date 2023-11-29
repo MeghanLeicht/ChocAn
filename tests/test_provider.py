79086afd978efc915d5da18a38fcdd283e5251fb
Vlad Chevdar <chevdar@cecs.pdx.edu>
2023-11-29 12:41:47 -0800

    same issues, record_service_billing

--- tests/test_provider.py
+++ tests/test_provider.py
@@ -10,29 +10,6 @@ from datetime import datetime
 from choc_an_simulator.provider import record_service_billing_entry
 
 
-def mock_prompt_date(message):
-    return datetime.strptime("11-26-2023", "%m-%d-%Y")
-
-
-def mock_prompt_int(message, char_limit, numeric_limit):
-    if "provider ID" in message:
-        return 123456789
-    elif "service code" in message:
-        return 555555
-
-
-mock_providers_df = pd.DataFrame({"provider_id": [123456789]})
-
-mock_services_df = pd.DataFrame(
-    {
-        "service_id": [555555],
-        "service_name": ["Nutrition Consultation"],
-        "price_dollars": [50],
-        "price_cents": [0],
-    }
-)
-
-
 class TestRecordServiceBillingEntry(unittest.TestCase):
     @patch("choc_an_simulator.provider.add_records_to_file")
     @patch("choc_an_simulator.provider.prompt_str")
@@ -50,14 +27,24 @@ class TestRecordServiceBillingEntry(unittest.TestCase):
         mock_prompt_date,
         mock_prompt_int_service_code,
         mock_load_records,
-        capsys,
     ):
+        mock_providers_df = pd.DataFrame({"provider_id": [123456789]})
+        mock_services_df = pd.DataFrame(
+            {
+                "service_id": [555555],
+                "service_name": ["Nutrition Consultation"],
+                "price_dollars": [50],
+                "price_cents": [0],
+            }
+        )
         mock_load_records.side_effect = [mock_providers_df, mock_services_df]
         mock_prompt_str_confirm.return_value = "yes"
         mock_prompt_str_comments.return_value = "Test comment"
+        mock_prompt_int_service_code.return_value = 555555
+        mock_prompt_int_provider_id.return_value = 123456789
+        mock_prompt_date.return_value = datetime.strptime("11-26-2023", "%m-%d-%Y")
 
         member_id = 987654321
-        record_service_billing_entry(member_id)
 
         original_stdout = sys.stdout
         sys.stdout = io.StringIO()
@@ -69,5 +56,4 @@ class TestRecordServiceBillingEntry(unittest.TestCase):
         sys.stdout = original_stdout
 
         self.assertIn("Service Billing Entry Recorded Successfully", output)
-
         mock_add_records.assert_called_once()
