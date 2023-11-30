d2cd361962b4413320f5d7079f7935b6d4a253ee
Danny Love <feldhaus@cecs.pdx.edu>
2023-11-29 20:55:00 -0800

    Fist draft of test_add_to_database

--- tests/system/test_manager_system.py
+++ tests/system/test_manager_system.py
@@ -1,16 +1,22 @@
 """System tests of the manager module."""
 import pytest
 from choc_an_simulator.login import login_menu
+from choc_an_simulator.database_management import load_records_from_file
+from choc_an_simulator.schemas import MEMBER_INFO, PROVIDER_DIRECTORY_INFO, USER_INFO
 
-MANAGER_LOGIN_SEQUENCE = ["333333333", "password"]
+MANAGER_LOGIN = ["333333333", "password"]
 
 
 class MenuPaths:
     """Collection of menu numbers corresponding to each option."""
 
-    MEMBER_REPORT = [4, 1]
-    PROVIDER_REPORT = [4, 2]
-    SUMMARY_REPORT = [4, 3]
+    MEMBER_REPORT = MANAGER_LOGIN + [4, 1]
+    PROVIDER_REPORT = MANAGER_LOGIN + [4, 2]
+    SUMMARY_REPORT = MANAGER_LOGIN + [4, 3]
+
+    ADD_MEMBER = MANAGER_LOGIN + [1, 1]
+    ADD_PROVIDER = MANAGER_LOGIN + [2, 1]
+    ADD_PROVIDER_DIRECTORY = MANAGER_LOGIN + [3, 1]
 
 
 @pytest.mark.parametrize(
@@ -32,5 +38,27 @@ def test_member_reports(
     with pytest.raises(StopIteration):
         login_menu()
     out = capsys.readouterr().out
-    print(out)
     assert expected_output in out
+
+
+@pytest.mark.parametrize(
+    "input_strs,table_info",
+    [
+        (MenuPaths.ADD_MEMBER + ["A", "B", "C", "OR", "97221"], MEMBER_INFO),
+        (MenuPaths.ADD_PROVIDER + ['A","B","C', "OR", "97221"], USER_INFO),
+        (MenuPaths.ADD_PROVIDER_DIRECTORY + ["S", "10", "10"], PROVIDER_DIRECTORY_INFO),
+    ],
+)
+@pytest.mark.usefixtures("mock_input_series", "save_example_info", "mock_report_dir")
+def test_add_to_database(
+    mock_input_series,
+    save_example_info,
+    capsys,
+    table_info,
+):
+    """Test adding a new member, provider, and service."""
+    df_before = load_records_from_file(table_info)
+    with pytest.raises(StopIteration):
+        login_menu()
+    df_after = load_records_from_file(table_info)
+    assert len(df_before) == len(df_after) - 1
