5270eae15a744fc8a0f645e144bfd58e6f79878d
Robert Elia <robertselia31@gmail.com>
2023-12-01 01:53:11 -0800

    Added summary report and tests. Updated request_provider_directory to work on windows as well. Added a definitions.py for some constants

--- src/choc_an_simulator/login.py
+++ src/choc_an_simulator/login.py
@@ -5,6 +5,10 @@ This module ensures secure access for providers and managers.
 It includes functions for display the login menu, generating a secure password,
 secure password verification, and user type authorization.
 """
+import bcrypt
+
+from choc_an_simulator.database_management import load_records_from_file
+from choc_an_simulator.schemas import USER_INFO
 
 
 def login_menu() -> None:
@@ -17,23 +21,27 @@ def login_menu() -> None:
     raise NotImplementedError("login_menu")
 
 
-def generate_secure_password(password: str) -> (bytes, str):
+def generate_secure_password(password: str) -> (bytes, bytes):
     """
     Generates a secure user password.
 
     This function is used to hash/salt the user password.
     It returns the hash/salt password and the salt.
     """
-    raise NotImplementedError("generate_secure_password")
+    password = password.encode()
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password, salt)
+    return hashed_password, salt
 
 
-def secure_password_verifiction(salt: str, hashed_password: str) -> bool:
+def secure_password_verification(user_id: int, password: str) -> bool:
     """
     Verifies the password entered by the user.
 
     Returns True or False if the password matches the database.
     """
-    raise NotImplementedError("secure_password_verification")
+    pw = load_records_from_file(USER_INFO, eq_cols={"id": user_id})["password_hash"]
+    return bcrypt.checkpw(pw, password.encode())
 
 
 def user_type_authorization() -> None:
